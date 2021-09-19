3e782d29c42d79fa73c72c654d53c5535cad0d7f
harisliv <livieratos.theoxaris@gmail.com>
2021-09-19 23:34:58 +0300

    Updates

---
+++ precache-manifest.e3f5108af803e4be9c8c12a085b9c0b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6dfcf1548108227b0e24363601994f6",
+    "url": "/mern-haris/index.html"
+  },
+  {
+    "revision": "3b5976ac95fdfc117ab4",
+    "url": "/mern-haris/static/css/main.1de46aec.chunk.css"
+  },
+  {
+    "revision": "27ffb164affa08d2db37",
+    "url": "/mern-haris/static/js/2.07e843b4.chunk.js"
+  },
+  {
+    "revision": "3b5976ac95fdfc117ab4",
+    "url": "/mern-haris/static/js/main.85f0c52c.chunk.js"
+  },
+  {
+    "revision": "a2389c871c3221e2b7f4",
+    "url": "/mern-haris/static/js/runtime-main.563b7079.js"
+  }
+]);
